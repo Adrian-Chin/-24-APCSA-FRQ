99237179f1ff07ffe647723dd12bffb5c30ba604
Adrian-Chin <adrianc159@nycstudents.net>
2024-06-03 11:51:10 -0400

    Update GridPath.java

--- FRQ-Solutions/Question-4/GridPath.java
+++ FRQ-Solutions/Question-4/GridPath.java
@@ -7,29 +7,35 @@ public class GridPath {
     this.grid = grid;
   }
 
-  // PART A 
+  // PART A (uncomment first)
   public Location getNextLoc(int row, int col) {
     if(row>=grid.length-1){
       return new Location(row, col+1);
     }else if(col>=grid[0].length-1){
       return new Location(row+1, col);
       }
+    
     if(grid[row+1][col]> grid[row][col+1]){
-  return new Location(row, col=1);
+  return new Location(row, col+1);
     }else{
   return new Location(row+1, col);
     }
   }
+  
 
+  
   // PART B
   public int sumPath(int row, int col) {
-  int total=grid[row][col];
-  Location result;
-    while(row<grid.length-1 || col<grid[0].length-1){
-      result=getNextLoc(row,col);
-      total+=grid[result.getRow()][result.getCol()];
-      row=result.getRow();
-      col=result.getCol();
+   int total=grid[row][col];
+   int r=row;
+   int c=col;
+
+    while(r!=grid.length-1 || c!=grid[0].length-1){
+   Location test=getNextLoc(r, c);
+      r= test.getRow();
+      c= test.getCol();
+      total+=grid[r][c];
+      
     }
     return total;
   }
